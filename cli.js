f8d4315dc8bb87bb32cff55ae19ac192fbc7d5f7
Annie Vo <avo2@unc.edu>
2023-02-08 23:40:30 -0500

    test

--- cli.js
+++ cli.js
@@ -42,7 +42,7 @@ if (args.e) {
 
 // Make a request
 
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&timezone=' + timezone + '&daily=precipitation_hours');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone=' + timezone)
 
 // Get the data from the request
 
